5cad97af813b64caafaf9954cd7d62f6cac0879c
HCCuong <102528939+HCCuong@users.noreply.github.com>
2023-05-27 15:48:24 +0700

    static file

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import configViewEngine from './configs/viewEngine'
+require('dotenv').config()
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+const path = require('path')
+
+configViewEngine(app)
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
