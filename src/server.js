6cf7bebc10761f81ff509d82bcb71567ae5e8b1b
HCCuong <102528939+HCCuong@users.noreply.github.com>
2023-05-31 21:02:18 +0700

    Middleware

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import configViewEngine from './configs/viewEngine'
 import initWebRoute from './route/web'
 import connection from './configs/connectDB'
 import initAPIRoute from './route/api'
+import multer from 'multer'
 
 require('dotenv').config()
+var morgan = require('morgan')
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 const path = require('path')
+
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +26,11 @@ initWebRoute(app)
 
 // Init API route
 initAPIRoute(app)
+
+// handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
